c84a71a49bd3e78149cf342d69abcb6e53fddcba
HoangLayor <giahoang481@gmail.com>
2026-05-14 12:08:16 +0700

    feature(db): Set up for MongoDB connection
    Add MongoDB connection lifespan into main

---
+++ backend/infra/mongodb/config.py
@@ -0,0 +1,17 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ENV_FILE_PATH = os.path.join(BASE_DIR, "config.env")
+
+class MongoDBSettings(BaseSettings):
+    MONGODB_URL: str = "mongodb://localhost:27017"
+    MONGODB_DB_NAME: str = "collaborative_db"
+
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE_PATH,
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+mongodb_settings = MongoDBSettings()
