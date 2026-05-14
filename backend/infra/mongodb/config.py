0b21fd4a821772e5e8d2d08a78789cf1d6afd592
HoangLayor <giahoang481@gmail.com>
2026-05-14 12:23:22 +0700

    refactor: move config.env to backend/.env

--- backend/infra/mongodb/config.py
+++ backend/infra/mongodb/config.py
@@ -1,8 +1,8 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ENV_FILE_PATH = os.path.join(BASE_DIR, "config.env")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ENV_FILE_PATH = os.path.join(BASE_DIR, ".env")
 
 class MongoDBSettings(BaseSettings):
     MONGODB_URL: str = "mongodb://localhost:27017"
