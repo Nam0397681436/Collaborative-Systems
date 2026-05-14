afead707e4d8fb1b0b937c45671b689143e3030f
Nam0397681436 <n0397681436@gmail.com>
2026-05-14 14:50:20 +0700

    code api tại gateway và push data lên rabbitmq

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
-
 from app.api.websocket import router as websocket_router
 from app.api.auth import router as auth_router
 from app.api.document import router as document_router
+from aio_pika import connect_robust,IncomingMessage
+import json
+import asyncio
+
 
 app = FastAPI(title="Collaborative Text Editor API")
 
@@ -16,10 +19,37 @@ app.add_middleware(
     expose_headers=["*"],
 )
 
-app.include_router(auth_router, prefix="/api/auth", tags=["Authentication"])
-app.include_router(document_router, prefix="/api/doc", tags=["Document Management"])
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+app.include_router(document_router, tags=["Document Management"])
 app.include_router(websocket_router, tags=["WebSockets"])
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to Collaborative Text Editor API Server"}
+
+async def listen_for_broadcast():
+    connection=await connect_robust("amqp://guest:guest@localhost/")
+    async with connection:
+        channel = await connection.channel()
+        exchange=await channel.declare_exchange(
+            name="ot_exchange",
+            type="fanout",
+            durable=True
+        )
+        queue = await channel.declare_queue(exclusive=True)
+        await queue.bind(exchange)
+
+        async with queue.iterator() as queue_iter:
+            async for message in queue_iter:
+                async with message.process():
+                    data = json.loads(message.body)
+                    doc_id = data.get("doc_id")
+                    
+                    # Gửi cho tất cả mọi người trong phòng (bao gồm cả người gõ)
+                    from app.api.websocket import connection_manager
+                    await connection_manager.broadcast_to_room(doc_id, data)
+    
+@app.on_event("startup")
+async def startup_event(background_tasks: BackgroundTasks):
+    # Chạy hàm lắng nghe như một task nền
+    background_tasks.add_task(listen_for_broadcast)
